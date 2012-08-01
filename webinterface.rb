0051b2b2bef49aa807f9e8d72aaa37a700130f7b
Fabian Schlenz <mail@fabianonline.de>
2012-08-01 23:48:08 +0200

    Buttons für nächste und vorige Sneak zum Webinterface hinzugefügt.

--- webinterface.rb
+++ webinterface.rb
@@ -5,6 +5,8 @@ class Webinterface < Sinatra::Base
 	
 	get '/?:id?' do
 		@sneak = Sneak.get(params[:id]) || Sneak.newest
+		@prev_sneak = Sneak.get(@sneak.id - 1)
+		@next_sneak = Sneak.get(@sneak.id + 1)
 		haml :main
 	end
 end
